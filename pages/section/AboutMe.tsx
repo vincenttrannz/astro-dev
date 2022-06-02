8a35dd670b23d0fdb50f55fa42296a30fb9667d1
Hieu Tran <tranhieunz@gmail.com>
2022-06-02 21:51:47 +1200

    Update: Added dangerouslySetInnerHTML to AboutMe

--- pages/section/AboutMe.tsx
+++ pages/section/AboutMe.tsx
@@ -11,6 +11,7 @@ interface AboutMe {
 }
 
 const AboutMe: React.FC<AboutMe> = ({ aboutDescription, aboutImage }) => {
+  console.log(aboutDescription);
   return (
     <div className="my-6 overflow-hidden" id="about">
       <div className="container mt-4 mb-8">
@@ -32,9 +33,8 @@ const AboutMe: React.FC<AboutMe> = ({ aboutDescription, aboutImage }) => {
           </AOSComp>
           <div className="col-md-6 py-3 py-md-6 position-relative">
             <AOSComp AOSAnimation="fade-left" AOSOffset="200" AOSDuration="1000" AOSRepeat={false}>
-              <ReactMarkdown>
-                { aboutDescription }
-              </ReactMarkdown>
+              <div dangerouslySetInnerHTML={{__html: aboutDescription}}>
+              </div>
               <a
                 className="button text-grey"
                 target="_blank"
