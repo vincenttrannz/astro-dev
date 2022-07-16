8a839795d46e262804e6d72e8e315b455b7cac20
Hieu Tran <tranhieunz@gmail.com>
2022-07-16 15:31:43 +1200

    Update: Added Footer move mountain js

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Link from 'next/link'
 
 type Props = {}
 
 const Footer = (props: Props) => {
+  const theMountain = useRef<HTMLDivElement>(null!);
+  const theMountainBorder = useRef<HTMLDivElement>(null!);
+  useEffect(() => {
+    const screenWidth = window.innerWidth;
+    const maxMove = 20;
+
+    window.onmousemove = (event) => {
+      const mouseX = event.pageX/screenWidth;	
+      const xMove = ((mouseX*maxMove)-maxMove);
+      theMountain.current.style.transform = `translateX(${xMove}px)`
+      theMountainBorder.current.style.transform = `translateX(${xMove}px)`
+      // theMountain.current.style.transform = 'rotateY('+yDegrees+'deg) rotateX('+xDegrees+'deg)';
+      // theMountainBorder.current.style.transform = 'rotateY('+yDegrees+'deg) rotateX('+xDegrees+'deg)';
+    }
+  }, [])
+
   return (
     <footer className='position-relative'>
       <div className='mountain-container'>
-        <div className='mountain-highlight-border'></div>
-        <div className='mountain-highlight'></div>
+        <div ref={theMountainBorder} className='mountain-highlight-border'></div>
+        <div ref={theMountain} className='mountain-highlight'></div>
         <div className='mountain-dim w-100 bg-blackBG z-index-99'></div>
       </div>
       <div className='pb-4 pt-3 pt-md-1 bg-blackBG z-index-99'>
